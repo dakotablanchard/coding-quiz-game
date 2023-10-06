4418aec257ec88d8084fa5699a31fd3f0bb3725c
Dakota Blanchard <dakotablanchard2699@gmail.com>
2023-10-06 13:27:13 -0400

    Got multiplpe choice buttons to appear on screen

--- README.md
+++ README.md
@@ -26,3 +26,8 @@ THEN I can save my initials and score -->
 
 [CSS Child Combinator](https://developer.mozilla.org/en-US/docs/Web/CSS/Child_combinator)
 [How To: Stop Countdown Timer](https://www.javatpoint.com/javascript-timer#:~:text=JavaScript%20offers%20two%20functions%20clearTimeout,stop%20the%20code%20execution%20beforehand.)
+
+https://www.w3schools.com/js/js_json.asp
+
+https://stackoverflow.com/questions/43118645/get-length-of-nested-javascript-object
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,76 @@
+var questionArray = [
+    {
+        "question": "How do you log somethine to the console?",
+        "choices": ["console.log()", "logToConsole()", "log.console()", "None of the above"],
+        "answer": "console.log()"
+    },
+
+    {
+        "question": "question 2",
+        "choices": ["choice 1", "choice 2", "choice 3", "choice 4"],
+        "answer": "answer 2"
+    },
+
+    {
+        "question": "question 3",
+        "choices": ["choice 1", "choice 2", "choice 3", "choice 4"],
+        "answer": "answer 3"
+    },
+
+    {
+        "question": "question 4",
+        "choices": ["choice 1", "choice 2", "choice 3", "choice 4"],
+        "answer": "answer 4"
+    },
+
+    {
+        "question": "question 5",
+        "choices": ["choice 1", "choice 2", "choice 3", "choice 4"],
+        "answer": "answer 5"
+    },
+
+    {
+        "question": "question 6",
+        "choices": ["im working", "choice 2", "choice 3", "choice 4"],
+        "answer": "answer 6"
+    },
+
+    {
+        "question": "question 7",
+        "choices": ["choice 1", "choice 2", "choice 3", "choice 4"],
+        "answer": "answer 7"
+    },
+
+    {
+        "question": "question 8",
+        "choices": ["choice 1", "choice 2", "choice 3", "choice 4"],
+        "answer": "answer 8"
+    },
+
+    {
+        "question": "question 9",
+        "choices": ["choice 1", "choice 2", "choice 3", "choice 4"],
+        "answer": "answer 9"
+    },
+
+    {
+        "question": "question 10",
+        "choices": ["choice 1", "choice 2", "choice 3", "choice 4"],
+        "answer": "answer 10"
+    },
+]
+
+var questionNumber = 0
+
 var timerNum = document.querySelector("#timer-digit")
 var homeScreen = document.querySelector(".home-screen")
+var multipleChoice = document.querySelector(".multiple-choice")
 
 var timeLimit = 60
 
 
 
-
-
-
-
-
-
-
-function beginCountdown() {
+function beginCountdown() { 
     var countdown = setInterval(function() {
         timerNum.textContent = timeLimit
         timeLimit--
@@ -23,9 +81,27 @@ function beginCountdown() {
 }
 
 function startGame() {
-    console.log("Game has started")
 
     beginCountdown()
 
     homeScreen.setAttribute ("style", "display: none;")
+
+    multipleChoiceQuestions()
+}
+
+function multipleChoiceQuestions() {
+console.log(questionArray[questionNumber].question)
+    var questionTitle = document.createElement("h2")
+        questionTitle.setAttribute("class", "question-title")
+        questionTitle.textContent = questionArray[questionNumber].question
+        multipleChoice.appendChild(questionTitle)
+
+    for (var i = 0; i < questionArray[questionNumber].choices.length; i++) {
+        var choiceButtons = document.createElement("button")
+        choiceButtons.setAttribute("class", "choice-buttons")
+        choiceButtons.setAttribute("id", "button" + i)
+        choiceButtons.textContent = questionArray[questionNumber].choices[i]
+        multipleChoice.appendChild(choiceButtons)
+
+    }
 }
